b41cc330b68367113c1b24ace409fca715d2da38
harsh-bhalwar <bhalwar14harsh05@gmail.com>
2024-11-21 12:34:03 +0530

    Initial Commit

---
+++ src/components/shared/SearchResults.tsx
@@ -0,0 +1,21 @@
+import { Models } from "appwrite";
+import Loader from "./Loader";
+import GridPostList from "./GridPostList";
+
+type SearchResultsProps = {
+  isSearchFetching: boolean;
+  searchedPosts: Models.Document[];
+}
+
+export const SearchResults = ({ isSearchFetching, searchedPosts }: SearchResultsProps) => {
+  if (isSearchFetching) {
+    return <Loader />;
+  } else if (searchedPosts && searchedPosts.documents.length > 0) {
+    return <GridPostList posts={searchedPosts.documents} />;
+  } else {
+    return (
+      <p className="text-light-4 mt-10 text-center w-full">No results found</p>
+    );
+  }
+};
+
